8e8f5914cb23a651c6640452b926de5397f6395e
47chen <shihchichen1010@vt.edu>
2022-01-14 19:22:17 -0500

    add more song

--- src/data.js
+++ src/data.js
@@ -5,7 +5,8 @@ function chillHop() {
   return [
     {
       name: "Turbulence",
-      cover: "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg",
       artist: "Parkbench Epiphany",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=27503",
       color: ["#f07f77", "#5d7784"],
@@ -64,7 +65,8 @@ function chillHop() {
     },
     {
       name: "Fireworks",
-      cover: "https://chillhop.com/wp-content/uploads/2020/07/46e61c111732cc3d72966225a1121a00bc693266-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/46e61c111732cc3d72966225a1121a00bc693266-1024x1024.jpg",
       artist: "Nymano, Pandrezz",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8479",
       color: ["#1d1b0f", "#c94043"],
@@ -73,7 +75,8 @@ function chillHop() {
     },
     {
       name: "You Should Have Known",
-      cover: "https://chillhop.com/wp-content/uploads/2020/10/ce88ec59bc8040bd6a4a23e4206ff0eed36813d8-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/ce88ec59bc8040bd6a4a23e4206ff0eed36813d8-1024x1024.jpg",
       artist: "Nymano, Pandress",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8838",
       color: ["#7c6454", "#c4af7f"],
@@ -82,7 +85,8 @@ function chillHop() {
     },
     {
       name: "Sun In My Face",
-      cover: "https://chillhop.com/wp-content/uploads/2020/10/ce88ec59bc8040bd6a4a23e4206ff0eed36813d8-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/10/ce88ec59bc8040bd6a4a23e4206ff0eed36813d8-1024x1024.jpg",
       artist: "Aso",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8330",
       color: ["#7c6454", "#c4af7f"],
@@ -91,25 +95,28 @@ function chillHop() {
     },
     {
       name: "False hope",
-      cover: "https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-1024x1024.jpg",
       artist: "Nymano Pandress",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10148",
-      color: ["#e7c8c8","#37478a"],
+      color: ["#e7c8c8", "#37478a"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Cozy Fire",
-      cover: "https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-1024x1024.jpg",
       artist: "xander.",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7955",
-      color: ["#e7c8c8","#37478a"],
+      color: ["#e7c8c8", "#37478a"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Jam Session",
-      cover: "https://chillhop.com/wp-content/uploads/2020/08/7ba352cafecae63aa3f09a52e125ea615d2c1f9b-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/08/7ba352cafecae63aa3f09a52e125ea615d2c1f9b-1024x1024.jpg",
       artist: "Montell Fish",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9003",
       color: ["#a2813c", "#cdf0fb"],
@@ -118,40 +125,84 @@ function chillHop() {
     },
     {
       name: "City Nights",
-      cover: "https://chillhop.com/wp-content/uploads/2020/07/bb0db71fd74f15627e9912ad2278c13cee72ac2d-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/bb0db71fd74f15627e9912ad2278c13cee72ac2d-1024x1024.jpg",
       artist: "stream error, H.1",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7852",
-      color: ["#5fa8a1","#feb87c"],
+      color: ["#5fa8a1", "#feb87c"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "I wish you would never stop showing",
-      cover: "https://chillhop.com/wp-content/uploads/2020/07/bb0db71fd74f15627e9912ad2278c13cee72ac2d-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/bb0db71fd74f15627e9912ad2278c13cee72ac2d-1024x1024.jpg",
       artist: "Sleepy Fish",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7900",
-      color: ["#feb87c","#5fa8a1"],
+      color: ["#feb87c", "#5fa8a1"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Metro Lines",
-      cover: "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
       artist: "Sleepy Fish",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7918",
-      color: ["#33b6c4","#fde1cc"],
+      color: ["#33b6c4", "#fde1cc"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Safe Travels!",
-      cover: "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/05/260a909fca701bbb7593f5f8f9cff9508cca2856-1024x1024.jpg",
       artist: "mommy",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=7941",
-      color: ["#33b6c4","#fde1cc"],
+      color: ["#33b6c4", "#fde1cc"],
       id: uuidv4(),
       active: false,
-    }
+    },
+    {
+      name: "Soulsounds",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/11/3ff29a35be582c8dc0222273cb9c401ea6b209dc-1024x1024.jpg",
+      artist: "Parkbench Epiphany",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=27500",
+      color: ["#a27615", "#becad8"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Waybackwhen",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/11/4c9682ee612a3b8ef51de286c49b5489408e9257-1024x1024.jpg",
+      artist: "Philanthrope, Parkbench, Epiphany",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=27502",
+      color: ["#f07f77", "#5d7784"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "for when it's warmer",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/d9cef192a40176c391f027231b399322ce91197d-1024x1024.jpg",
+      artist: "Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8233",
+      color: ["#c66137", "#55afbb"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Man I miss my cat",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2021/09/2ce75252f5419a45d76bb93424ac1eae3e688b17-1024x1024.jpg",
+      artist: "Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=24828",
+      color: ["#feaf87", "#bae5fb"],
+      id: uuidv4(),
+      acitve: false,
+    },
     //ADD MORE HERE, WHATEVER YOU WANT
   ];
 }
